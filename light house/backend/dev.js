0bf9cf86aabcdefde2df0889cbbf962a3a35ae34
lighthouse-bit <92857385+lighthouse-bit@users.noreply.github.com>
2025-10-17 21:25:54 +0100

    Delete light house directory

--- light house/backend/dev.js
+++
@@ -1,7 +0,0 @@
-// dev.js
-import app from "./server.js";
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running locally at http://localhost:${PORT}`);
-});
